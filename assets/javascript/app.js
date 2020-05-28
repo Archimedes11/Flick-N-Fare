cbd808a9f742ca4c3b31519bafe5d0096fc5276f
cjohnson1580 <cjohnson1580@gmail.com>
2020-05-28 10:15:38 -0500

    pull from git

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,118 +1,86 @@
-
-// Your web app's Firebase configuration
-var firebaseConfig = {
-    apiKey: "AIzaSyBxS26xgOho5G_1W-Cc9Rwx6cmr5gNfcA0",
-    authDomain: "a-new-project-a5553.firebaseapp.com",
-    databaseURL: "https://a-new-project-a5553.firebaseio.com",
-    projectId: "a-new-project-a5553",
-    storageBucket: "a-new-project-a5553.appspot.com",
-    messagingSenderId: "582759574527",
-    appId: "1:582759574527:web:4a5e53e17b3e6fcb8515ca",
-    measurementId: "G-KT0DPHG31D"
+var randomId = 0;
+function fareSearch() {
+  pickFare();
+  var settings1 = {
+    url:
+      "https://api.spoonacular.com/recipes/" +
+      randomId +
+      "/information?cuisine=" +
+      fare +
+      "&offset=50&number=1&instructionsRequired=true&includeNutrition=true&apiKey=c4a805b12c474690b2cb2c967cd12dff",
+    method: "GET",
+    timeout: 0,
+    headers: {},
   };
-  // Initialize Firebase
-  firebase.initializeApp(firebaseConfig);
-  firebase.analytics();
-
-
-
-//Recipe Search
-var settings1 = {
-  async: true,
-  crossDomain: true,
-  url:
-    "https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/search?cuisine=indian&number=10&type=main%20course&query=burger",
-  method: "GET",
-  headers: {
-    "x-rapidapi-host": "spoonacular-recipe-food-nutrition-v1.p.rapidapi.com",
-    "x-rapidapi-key": "62afd12ed3msh967bb13d2fe4a21p1bc52fjsn999fe4d47451",
-  },
-};
-
-$.ajax(settings).done(function (response) {
-  console.log(response);
-});
-//Get Similar Recipes
-var settings2 = {
-  async: true,
-  crossDomain: true,
-  url:
-    "https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/156992/similar",
-  method: "GET",
-  headers: {
-    "x-rapidapi-host": "spoonacular-recipe-food-nutrition-v1.p.rapidapi.com",
-    "x-rapidapi-key": "62afd12ed3msh967bb13d2fe4a21p1bc52fjsn999fe4d47451",
-  },
-};
-
-$.ajax(settings).done(function (response) {
-  console.log(response);
-});
-//Get Recipe Nutrition by ID
-var settings3 = {
-  async: true,
-  crossDomain: true,
-  url:
-    "https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/visualizeNutrition",
-  method: "POST",
-  headers: {
-    "x-rapidapi-host": "spoonacular-recipe-food-nutrition-v1.p.rapidapi.com",
-    "x-rapidapi-key": "62afd12ed3msh967bb13d2fe4a21p1bc52fjsn999fe4d47451",
-    accept: "text/html",
-    "content-type": "application/x-www-form-urlencoded",
-  },
-  data: {
-    ingredientList: "3 oz flour",
-    servings: "2",
-  },
-};
-
-
-$.ajax(settings).done(function (response) {
-  console.log(response);
-});
-
-var fareArray = [];
-var Italian = "Italian";
+  $.ajax(settings1).then(function (response) {
+    $("#farePic").attr("src", response.image);
+    $("#recipeTitle").text(response.title);
+    $("#recipe").html(response.summary);
+    function getFare() {
+      $("#recipeTitle").attr("src", "url(" + response + ")");
+    }
+    getFare();
+  });
+}
+//Diet Choice
+function getRandomFare() {
+  pickFare();
+  var settings2 = {
+    url:
+      "https://api.spoonacular.com/recipes/search?diet=" +
+      fare +
+      "&offset=50&number=1&instructionsRequired=<boolean>&apiKey=c4a805b12c474690b2cb2c967cd12dff",
+    method: "GET",
+    timeout: 0,
+    headers: {},
+  };
+  console.log(settings2.url);
+  $.ajax(settings2).then(function (response) {
+    //console.log(response);
+    for (var m = 0; m < response.results.length; m++) {
+      randomId = response.results[m].id;
+      console.log(settings2.url);
+      fareSearch();
+    }
+  });
+}
+console.log("random id: " + randomId);
+var fare;
+var Italian = "italian";
 var American = "American";
 var Mexican = "Mexican";
 var Indian = "Indian";
-var Vegan = "Vegan";
 var Cajun = "Cajun";
-var Mediterrean = "Mediterrean";
-
+var Greek = "Greek";
+var dietChoiceArray = [];
+var GlutenFree = "Gluten Free";
+var Vegan = "Vegan";
 function pickFare() {
-	if ($("#selectFare").val() === "Italian") {
-	  genre = Italian;
-	}
-	if ($("#select").val() === "American") {
-	  genre = American;
-	}
-	if ($("#select").val() === "Mexican") {
-	  genre = Mexican;
-	}
-	if ($("#select").val() === "Indian") {
-	  genre = Indian;
-	}
-	if ($("#select").val() === "Vegan") {
-	  genre = Vegan;
-	}
-	if ($("#select").val() === "Cajun") {
-	  genre = Cajun;
-	}
-	if ($("#select").val() === "Mediterrean") {
-	  genre = Mediterrean;
-	}
+  if ($("#selectFare").val() === "Italian") {
+    fare = Italian;
   }
-
-  function fareSearch() {
-	//var fare = $(this).attr("value");
-	pickFare();
-	var queryURL =
-  "https://spoonacular.com/food-api/docs#Authentication&apiKey=c4a805b12c474690b2cb2c967cd12dff"+
-	  fare;
-	console.log(fare);
-
+  if ($("#selectFare").val() === "American") {
+    fare = American;
+  }
+  if ($("#selectFare").val() === "Mexican") {
+    fare = Mexican;
+  }
+  if ($("#selectFare").val() === "Indian") {
+    fare = Indian;
+  }
+  if ($("#selectFare").val() === "Vegan") {
+    fare = Vegan;
+  }
+  if ($("#selectFare").val() === "Cajun") {
+    fare = Cajun;
+  }
+  if ($("#selectFare").val() === "Greek") {
+    fare = Greek;
+  }
+  if ($("#selectFare").val() === "GlutenFree") {
+    fare = GlutenFree;
+  }
+}
 var movieArray = [];
 var action = 28;
 var comedy = 35;
@@ -122,7 +90,6 @@ var family = 10751;
 var horror = 27;
 var scifi = 878;
 var genre = 0;
-
 function pickGenre() {
   if ($("#select").val() === "action") {
     genre = action;
@@ -146,7 +113,6 @@ function pickGenre() {
     genre = scifi;
   }
 }
-
 function movieSearch() {
   //var genre = $(this).attr("value");
   pickGenre();
@@ -154,50 +120,7 @@ function movieSearch() {
     "https://api.themoviedb.org/3/discover/movie?api_key=ff46f8ea1d82a3eb64afbd0bbaf6cef5&include_adult=false&with_genres=" +
     genre;
   console.log(genre);
-
- 
-//test loop
-
-for (i = 0; i < 5; i++) {
-    console.log(i);
- 
-	}
-
-	$.ajax({
-		url: queryURL,
-		method: "GET",
-	  }).then(function (response) {
-		for (var i = 0; i < response.results.length; i++) {
-		  //console.log(response.results[i]);
-		  var results = response.results[i];
-		  movieArray.push(results);
-		}
-		//console.log(movieArray);
-		function getMovie() {
-		  var randomMovie =
-			movieArray[Math.floor(Math.random() * movieArray.length)];
-		  console.log(randomMovie.poster_path);
-		  $("#moviePoster").attr(
-			"src",
-			"https://image.tmdb.org/t/p/original/" + randomMovie.poster_path
-		  );
-		  console.log(randomMovie.title);
-		  $("#title").text(randomMovie.title);
-		  console.log(randomMovie.release_date);
-		  $("#releaseDate").text(randomMovie.release_date);
-		  console.log(randomMovie.overview);
-		  $("#synopsis").text(randomMovie.overview);
-		}
-		getMovie();
-	  });
-	}
-	
-	$("#result").on("click", function () {
-	  movieSearch();
-	});
-
-
-	$.ajax({
+  $.ajax({
     url: queryURL,
     method: "GET",
   }).then(function (response) {
@@ -221,11 +144,23 @@ for (i = 0; i < 5; i++) {
       $("#releaseDate").text(randomMovie.release_date);
       console.log(randomMovie.overview);
       $("#synopsis").text(randomMovie.overview);
+      console.log("Genre => " + randomMovie.genre_ids);
+      localStorage.setItem("movie", randomMovie.title);
+      $(movieHistory).text(localStorage.getItem("movie"));
     }
     getMovie();
   });
 }
-
+var movieHistory = $("<p>");
+movieHistory.css({
+  color: "blue",
+  "font-size": "14px",
+});
+$("#movieHistoryText").append(movieHistory);
+$(movieHistory).text(localStorage.getItem("movie"));
 $("#result").on("click", function () {
   movieSearch();
+  //console.log(movieHistory + "Text");
+  getRandomFare();
 });
+$(movieHistory).on("click", function () { });
